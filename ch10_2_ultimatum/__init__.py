c51bf9f17eab5c40448bf67443d9b76f08481f2d
八木遥菜 <cgej0939@mail3.doshisha.ac.jp>
2025-07-08 02:18:43 +0900

    Update __init__.py

--- ch10_2_ultimatum/__init__.py
+++ ch10_2_ultimatum/__init__.py
@@ -1,9 +1,7 @@
 from otree.api import *
 import random
 
-
 doc = """最後通牒ゲーム"""
-
 
 class C(BaseConstants):
     NAME_IN_URL = "ch10_2_ultimatum"
@@ -43,6 +41,7 @@ class C(BaseConstants):
 
     CHOICE_LIST_GOTE = [[0, "承諾する"], [1, "拒否する"]]
 
+SESSION_FIELDS = ['graph_data']
 
 class Subsession(BaseSubsession):
     num_participants_p1 = models.IntegerField(initial=0)
@@ -61,7 +60,6 @@ class Subsession(BaseSubsession):
     num_accept = models.IntegerField(initial=0)
     num_reject = models.IntegerField(initial=0)
 
-    #
     num_participants = models.IntegerField(initial=0)
     num_10_accept = models.IntegerField(initial=0)
     num_10_reject = models.IntegerField(initial=0)
@@ -89,7 +87,6 @@ class Subsession(BaseSubsession):
     err_message = models.StringField()
     err_message_pair = models.StringField()
 
-
 class Group(BaseGroup):
     p1_amount = models.IntegerField()
     p2_amount = models.IntegerField()
@@ -109,9 +106,7 @@ class Group(BaseGroup):
     flg_non_input_p1 = models.IntegerField(initial=0)
     flg_non_input_p2 = models.IntegerField(initial=0)
 
-
 class Player(BasePlayer):
-    # 戦略法
     tmp_first_player = models.StringField(
         choices=C.CHOICE_LIST_SENTE, widget=widgets.RadioSelect, label=""
     )
@@ -122,15 +117,12 @@ class Player(BasePlayer):
     )
     tmp_second_player_why = models.LongStringField(label="なぜその選択をしましたか？")
 
-
-# FUNCTIONS
 def set_P1(player: Player):
     sub = player.subsession
     group = player.group
 
     if group.p1_decision != "":
         sub.num_participants_p1 += 1
-        print("++++++++++++++++", group.p1_decision)
         s = group.p1_decision
         if s == "10":
             sub.num_10 += 1
@@ -160,18 +152,14 @@ def set_P1(player: Player):
         group.flg_non_input_p1 = 1
         tmp = random.randint(0, 10)
         group.p1_decision = str(C.CHOICE_LIST_SENTE[tmp][0])
-    # 先手の配分計算
-    # group.p1_amount = C.ENDOWMENT - int(group.p1_decision)
     group.p1_amount = int(group.p1_decision)
     group.p2_amount = C.ENDOWMENT - int(group.p1_decision)
-
 
 def set_P2(player: Player):
     sub = player.subsession
     group = player.group
     s = group.p2_decision
     if s != "":
-        # グラフ用集計
         sub.num_participants_p2 += 1
         if s == "0":
             sub.num_accept += 1
@@ -183,7 +171,6 @@ def set_P2(player: Player):
         group.flg_non_input_p2 = 1
         tmp = random.randint(0, 1)
         group.p2_decision = str(C.CHOICE_LIST_GOTE[tmp][0])
-
 
 def set_pair(player: Player):
     sub = player.subsession
@@ -206,7 +193,7 @@ def set_pair(player: Player):
     elif p1 == "8" and p2 == "1":
         sub.num_8_reject += 1
     elif p1 == "7" and p2 == "0":
-        sub.num_700_accept += 1
+        sub.num_7_accept += 1
     elif p1 == "7" and p2 == "1":
         sub.num_7_reject += 1
     elif p1 == "6" and p2 == "0":
@@ -240,174 +227,58 @@ def set_pair(player: Player):
     else:
         sub.err_message_pair = "エラーあり"
 
-
 def set_P2s(subsession: Subsession):
     for p in subsession.get_players():
         set_P2(p)
 
-
 def set_P1s(subsession: Subsession):
     for p in subsession.get_players():
         set_P1(p)
-
 
 def set_pairs(subsession: Subsession):
     for p in subsession.get_players():
         set_pair(p)
     graph(subsession=subsession)
 
-
 def set_payoffs(group: Group):
     p1 = group.get_player_by_id(1)
     p2 = group.get_player_by_id(2)
 
-    # 0：承諾
     if group.p2_decision == "0":
-        # p1.payoff = C.ENDOWMENT - int(group.p1_decision)
-        # p2.payoff = int(group.p1_decision)
         p1.payoff = int(group.p1_decision)
         p2.payoff = C.ENDOWMENT - int(group.p1_decision)
-    # 1：拒否
     else:
         p1.payoff = 0
         p2.payoff = 0
 
-
-# グラフ描画用
 def graph(subsession: Subsession):
     sub = subsession
     session = sub.session
     graph_list_accept = []
     graph_list_reject = []
-
     ch10_2_result = []
 
-    # 割合に計算(accept)
-    if sub.num_0_reject > 0:
-        tmp = round((sub.num_0_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-    if sub.num_1_reject > 0:
-        tmp = round((sub.num_1_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-    if sub.num_2_reject > 0:
-        tmp = round((sub.num_2_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-    if sub.num_3_reject > 0:
-        tmp = round((sub.num_3_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-    if sub.num_4_reject > 0:
-        tmp = round((sub.num_4_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-    if sub.num_5_reject > 0:
-        tmp = round((sub.num_5_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-    if sub.num_6_reject > 0:
-        tmp = round((sub.num_6_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-    if sub.num_7_reject > 0:
-        tmp = round((sub.num_7_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-    if sub.num_8_reject > 0:
-        tmp = round((sub.num_8_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-    if sub.num_9_reject > 0:
-        tmp = round((sub.num_9_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-    if sub.num_10_reject > 0:
-        tmp = round((sub.num_10_reject / sub.num_participants) * 100, 2)
-        graph_list_reject.append(tmp)
-    else:
-        graph_list_reject.append(0)
-
-    # accept
-    if sub.num_0_accept > 0:
-        tmp = round((sub.num_0_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-    if sub.num_1_accept > 0:
-        tmp = round((sub.num_1_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-    if sub.num_2_accept > 0:
-        tmp = round((sub.num_2_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-    if sub.num_3_accept > 0:
-        tmp = round((sub.num_3_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-    if sub.num_4_accept > 0:
-        tmp = round((sub.num_4_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-    if sub.num_5_accept > 0:
-        tmp = round((sub.num_5_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-    if sub.num_6_accept > 0:
-        tmp = round((sub.num_6_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-    if sub.num_7_accept > 0:
-        tmp = round((sub.num_7_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-    if sub.num_8_accept > 0:
-        tmp = round((sub.num_8_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-    if sub.num_9_accept > 0:
-        tmp = round((sub.num_9_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-    if sub.num_10_accept > 0:
-        tmp = round((sub.num_10_accept / sub.num_participants) * 100, 2)
-        graph_list_accept.append(tmp)
-    else:
-        graph_list_accept.append(0)
-
-    print(graph_list_accept, graph_list_reject)
+    # 割合計算(拒否)
+    for i in range(0, 11):
+        reject_count = getattr(sub, f'num_{i}_reject')
+        if reject_count > 0:
+            tmp = round((reject_count / sub.num_participants) * 100, 2)
+            graph_list_reject.append(tmp)
+        else:
+            graph_list_reject.append(0)
+    # 割合計算(承諾)
+    for i in range(0, 11):
+        accept_count = getattr(sub, f'num_{i}_accept')
+        if accept_count > 0:
+            tmp = round((accept_count / sub.num_participants) * 100, 2)
+            graph_list_accept.append(tmp)
+        else:
+            graph_list_accept.append(0)
 
     for player in subsession.get_players():
         group = player.group
         participant = player.participant
-
-        p2_decision = ""
-        if group.p2_decision == "0":
-            p2_decision = "承諾"
-        else:
-            p2_decision = "拒否"
-
+        p2_decision = "承諾" if group.p2_decision == "0" else "拒否"
         participant.vars["ch10_2_result"] = (
             "++++++++++++++++++++++++++++++++++++++++++++++++++<br>"
             "最後通牒ゲーム：あなたの結果："
@@ -419,24 +290,20 @@ def graph(subsession: Subsession):
             "プレイヤー2（後手）は、プレイヤー1の提案を" + (p2_decision) + "しました。<br>"
             "++++++++++++++++++++++++++++++++++++++++++++++++++"
         )
+        ch10_2_result.append(participant.vars["ch10_2_result"])
 
-    ch10_2_result.append(participant.vars["ch10_2_result"])
-
-    # 最終結果用
+    # 最終結果用（修正点！session.varsを使うこと）
     if "graph_data" not in session.vars:
-        session.graph_data = {}
-    session.graph_data["ch10_2"] = {
+        session.vars["graph_data"] = {}
+    session.vars["graph_data"]["ch10_2"] = {
         "num_participants": sub.num_participants,
         "graph_list_accept": graph_list_accept,
         "graph_list_reject": graph_list_reject,
         "ch10_2_result": ch10_2_result,
     }
 
-
-# PAGES-----
 class Introduction(Page):
     timeout_seconds = 60
-
 
 class Strategy(Page):
     timeout_seconds = 120 + C.ADD_TIME
@@ -448,9 +315,7 @@ class Strategy(Page):
         "tmp_second_player_why",
     ]
 
-
 class Send(Page):
-    # timeout_seconds = 60
     timeout_seconds = C.ADD_TIME
     form_model = "group"
     form_fields = ["p1_decision", "p1_decision_why"]
@@ -459,14 +324,11 @@ class Send(Page):
     def is_displayed(player: Player):
         return player.id_in_group == 1
 
-
 class WaitForP1(WaitPage):
     wait_for_all_groups = True
     after_all_players_arrive = set_P1s
 
-
 class SendBack(Page):
-    # timeout_seconds = 60
     timeout_seconds = C.ADD_TIME
     form_model = "group"
     form_fields = ["p2_decision", "p2_decision_why"]
@@ -475,27 +337,23 @@ class SendBack(Page):
     def is_displayed(player: Player):
         return player.id_in_group == 2
 
-
 class WaitForP2(WaitPage):
     wait_for_all_groups = True
     after_all_players_arrive = set_P2s
 
-
 class ResultsWaitPage(WaitPage):
     after_all_players_arrive = set_payoffs
-
 
 class ResultWaitPair(WaitPage):
     wait_for_all_groups = True
     after_all_players_arrive = set_pairs
 
-
 class Results(Page):
     @staticmethod
     def js_vars(player: Player):
         print("js_vars")
-        return player.session.graph_data
-
+        # session.graph_data ではなく session.vars['graph_data']
+        return player.session.vars.get("graph_data", {})
 
 page_sequence = [
     Introduction,
